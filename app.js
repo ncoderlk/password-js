0c26a1183a0aff147aff56f2a8ec4cc868cc8a4f
Sithika Dangampala <105375428+ncoderlk@users.noreply.github.com>
2022-05-22 19:58:55 +0530

    Updated
    app.js comments added

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const characters=capitalLetter+number+symbol+simpleLetter;
 var Passlength=15;
 //----------------------------
 var Password="";
+//Process
 for(i=0;i<Passlength;i++){
     var randomPass=Math.floor(Math.random()*characters.length)
     Password+=characters.substring(randomPass,randomPass+1)
 }
+//Display
 console.log(Password);
